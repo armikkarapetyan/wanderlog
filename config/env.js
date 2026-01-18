b0d88f8c9a534cd94c92dcc1e311b04ae31e8e33
Armik <tnearmush@gmail.com>
2026-01-18 10:10:30 +0400

    Removed duplicates

--- config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const env = {
-  PORT: process.env.APP_PORT || 4000,
-  RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
-  RAPIDAPI_HOST: process.env.RAPIDAPI_HOST,
-};
--- routes/hotels.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-import hotelsController from '../controllers/hotelsController.js'
-export const hotelRouter = express.Router()
-
-hotelRouter.get("/hotels", hotelsController.getHotels)
